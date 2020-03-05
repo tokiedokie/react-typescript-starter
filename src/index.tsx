b49055974806e8270499f79db85363a8e43b83b0
tokiedokie <42903136+tokiedokie@users.noreply.github.com>
2020-03-05 12:48:20 +0900

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,16 @@
-import * as React from "react";
-import * as ReactDOM from "react-dom";
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
 
 interface HelloProps {
-  compiler: string;
-  framework: string;
+  compiler: string
+  framework: string
 }
 
-export class Hello extends React.Component<HelloProps, {}> {
-  render() {
-    return <h1>Hello from {this.props.compiler} and {this.props.framework}!</h1>;
-  }
+const Hello: React.FC<HelloProps> = (props) => {
+  return <h1>Hello from {props.compiler} and {props.framework}!</h1>
 }
 
 ReactDOM.render(
   <Hello compiler="TypeScript" framework="React" />,
-  document.getElementById("root")
-);
+  document.getElementById('root')
+)
